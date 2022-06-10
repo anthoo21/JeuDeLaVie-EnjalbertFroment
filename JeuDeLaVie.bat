324d207214d5b8d49c7137ca5021db565a39a787
anthoo21 <90391050+anthoo21@users.noreply.github.com>
2022-06-10 11:28:06 +0200

    Add files via upload

---
+++ JeuDeLaVie.bat
@@ -0,0 +1,2 @@
+set path=FichierPourExecutable\jdk-17\bin
+java --module-path "FichierPourExecutable\javafx-sdk-18\lib" --add-modules javafx.controls,javafx.fxml -jar "FichierPourExecutable\JeuDeLaVie.jar"
