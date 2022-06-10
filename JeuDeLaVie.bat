b6408646b24d6aee296ae566cf9ee550888d6d67
anthoo21 <90391050+anthoo21@users.noreply.github.com>
2022-06-10 11:36:11 +0200

    Delete JeuDeLaVie.bat

--- JeuDeLaVie.bat
+++
@@ -1,2 +0,0 @@
-set path=FichierPourExecutable\jdk-17\bin
-java --module-path "FichierPourExecutable\javafx-sdk-18\lib" --add-modules javafx.controls,javafx.fxml -jar "FichierPourExecutable\JeuDeLaVie.jar"
